e0fc38a49428d6a36b273b1ea8e8d6d87449cfe6
Dr.-Ing. Chukwuka Victor obionwu <victordeman@users.noreply.github.com>
2026-01-07 11:59:42 +0100

    Create user.py

---
+++ backend/app/models/user.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from app.models import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    # Add more fields as needed
