bd1716b5f78f67168dd79457a0c15f3cb75b3e8e
Dr.-Ing. Chukwuka Victor obionwu <victordeman@users.noreply.github.com>
2026-01-07 14:38:33 +0100

    Create GoogleDrivePicker.jsx

---
+++ frontend/src/components/GoogleDrivePicker.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// Implement Google Picker API
+// https://developers.google.com/picker/docs
+
+const GoogleDrivePicker = ({ onSelect }) => {
+  // Load Google API and show picker
+  return (
+    <button onClick={() => {/* show picker */}}>Connect Google Drive</button>
+  );
+};
+
+export default GoogleDrivePicker;
