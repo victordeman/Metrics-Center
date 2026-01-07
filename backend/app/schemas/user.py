a15a88b976d53e06265cc68d261b8e48452477ad
Dr.-Ing. Chukwuka Victor obionwu <victordeman@users.noreply.github.com>
2026-01-07 12:00:56 +0100

    Create user.py

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+class UserBase(BaseModel):
+    email: str
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
