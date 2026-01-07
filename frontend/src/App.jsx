3235fabe488146426f9ae7001f23235cd9ca81a1
Dr.-Ing. Chukwuka Victor obionwu <victordeman@users.noreply.github.com>
2026-01-07 14:44:40 +0100

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import ProjectDetail from './pages/ProjectDetail';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects/:id" element={<ProjectDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
